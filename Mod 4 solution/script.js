c3ff5aecf59cbeeec6f699d07528b9878330faf0
Rewash-Chettri <63732130+Rewash-Chettri@users.noreply.github.com>
2020-05-17 11:25:24 +0530

    Update script.js

--- Mod 4 solution/script.js
+++ Mod 4 solution/script.js
@@ -1,7 +1,7 @@
 
 
 (function() {
-    var names = ["Tokyo", "Berlin", "Jasmine", "Rio", "Nairobi", "Helsinki", "Denver", "Jenny", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
